0d5c1ca9ddb8a98a4deaabef230fba1ed327012c
Bianca Behm <bbehm@student.hive.fi>
2020-11-11 18:15:50 +0200

    formatting

--- day02/ex01/one_more_time.php
+++ day02/ex01/one_more_time.php
@@ -5,7 +5,7 @@ if ($argc > 1) {
 	$format = preg_match('/^([Ll]undi|[Mm]ardi|[Mm]ercredi|[Jj]eudi|[Vv]endredi|[Ss]amedi|[Dd]imanche) ([0-9]|1[0-9]|2[0-9]|3[0-1]) ([Jj]anvier|[Ff]evrier|[Mm]ars|[Aa]vril|[Mm]ai|[Jj]uin|[Jj]uillet|[Aa]out|[Ss]eptembre|[Oo]ctobre|[Nn]ovembre|[Dd]ecembre) ([0-9]{4}) (([01][0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9]))$/', $argv[1], $matches);
 	if (!$format) {
 		echo "Wrong Format\n";
-		exit ;
+		exit;
 	}
 
 	$months = ['janvier', 'fevrier', 'mars', 'avril', 'mai', 'juin', 'juillet', 'aout', 'septembre', 'octobre', 'novembre', 'decembre'];
@@ -13,18 +13,16 @@ if ($argc > 1) {
 		$month = $month + 1;
 	} else if (!$month) {
 		echo "Wrong Format\n";
-		exit ;
+		exit;
 	}
 
-	$time = $matches[4].'-'.$month.'-'.$matches[2].' '.$matches[5];
+	$time = $matches[4] . '-' . $month . '-' . $matches[2] . ' ' . $matches[5];
 	$time = strtotime($time);
 	if (!$time) {
 		echo "Wrong Format\n";
-		exit ;
+		exit;
 	}
-	echo $time."\n";
+	echo $time . "\n";
 }
 exit;
 ?>
-
-
