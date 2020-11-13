9f8e7da3600246c6da91607f452352a921e2e411
Bianca Behm <bbehm@student.hive.fi>
2020-11-13 22:25:47 +0200

    ex01 and ex02

---
+++ day04/ex01/create.php
@@ -0,0 +1,28 @@
+<?php
+// allowed functions: hash(), file_get_contents(), file_put_contents(), serialize(), unserialize(), file_exists(), mkdir()
+if ($_POST['submit'] == 'OK' && $_POST['login'] && $_POST['passwd']) {
+	$dir_path = '../htdocs/';
+	if (!file_exists($dir_path)) {
+		mkdir($dir_path);
+	}
+	if (!file_exists($dir_path . 'private')) {
+		mkdir($dir_path . 'private');
+	}
+	$file = $dir_path . 'private/passwd';
+	if (file_exists($file)) {
+		$user_info = unserialize(file_get_contents($file));
+	}
+	foreach ($user_info as $user) {
+		if ($user['login'] == $_POST['login']) {
+			exit("ERROR\n");
+		}
+	}
+	$new_user = array('login' => $_POST['login'], 'passwd' => hash('whirlpool', $_POST['passwd']));
+	$user_info[] = $new_user;
+	file_put_contents($file, serialize($user_info));
+	echo "OK\n";
+
+} else {
+	exit("ERROR\n");
+}
+?>
