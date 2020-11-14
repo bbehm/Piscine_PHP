adabc751addf8b20210072358f72cf1df9e0f72d
Bianca Behm <54929608+bbehm@users.noreply.github.com>
2020-11-14 19:47:17 +0200

    fixed typo

--- day04/ex00/index.php
+++ day04/ex00/index.php
@@ -2,14 +2,14 @@
 session_start();
 if ($_GET['submit'] == "OK") {
 	$_SESSION['login'] = $_GET['login'];
-	$_SESSION['pwsswd'] = $_GET['passwd'];
+	$_SESSION['passwd'] = $_GET['passwd'];
 }
 ?>
 <html><body>
 	<form action="index.php" method="GET">
-		Username: <input type="text" name="login" value=<?php echo $_SESSION['login']; ?> />
+		Username: <input type="text" name="login" value=<?= $_SESSION['login']; ?> />
 		<br />
-		Password: <input type="password" name="passwd" value=<?php echo $_SESSION['passwd']; ?> />
+		Password: <input type="password" name="passwd" value=<?= $_SESSION['passwd']; ?> />
 		<input type="submit" name="submit" type="OK"/>
 	</form>
 </body></html>
